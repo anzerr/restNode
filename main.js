7dc17aa047be28d4b97213af42d1c31fcec25ade
anzerr <ryan.anzer@epitech.eu>
2015-09-09 18:17:53 +0200

    main

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 
 var http = require('http');
-var server = http.createServer(function (req, res) {
+http.createServer(function (req, res) {
   res.writeHead(200);
   res.end('Ok\n');
-});
-server.listen(80, '0.0.0.0');
+}).listen(80);
