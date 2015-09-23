685793e4dc6e235c22550a5ab7f1a773c4cebd59
anzerr <ryan.anzer@epitech.eu>
2015-09-23 19:30:08 +0200

    fuck

--- remove.js
+++ remove.js
@@ -3,7 +3,7 @@ module.exports = function(url, handle, req, res) {
 	var path = url.split('/');
 	console.log(path);
 	
-	handle.query('DELETE FROM posts WHERE title = ' + handle.escapeId(path[path.length - 1]), function(error, result) {
+	handle.query('DELETE FROM user WHERE id = ' + Number(path[path.length - 1]), function(error, result) {
 		if (!error && result.affectedRows !== 0) {
 			res.writeHead(200, {'Content-Type': 'application/json'});
 			res.end(JSON.stringify({
