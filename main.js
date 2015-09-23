221aa929581f20b3cecaec8231c6be3922b0de10
anzerr <ryan.anzer@epitech.eu>
2015-09-23 17:14:53 +0200

    cat

--- main.js
+++ main.js
@@ -20,6 +20,7 @@ var auth = function(req, res, handle, callback) {
 	} else if(auth) {    // The Authorization was passed in so now we validate it
 		var tmp = auth.split(' ');   // Split on a space, the original auth looks like  "Basic Y2hhcmxlczoxMjM0NQ==" and we need the 2nd part
 
+		console.log(tmp);
 		var buf = new Buffer(tmp[1], 'base64'); // create a buffer and tell it the data coming in is base64
 		var plain_auth = buf.toString();        // read it back out as a string
 
